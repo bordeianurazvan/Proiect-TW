e63c259ed2a832185bd8b2fa5e033bed012f71e7
george-tifrea <georgetifrea@gmail.com>
2017-05-28 20:36:27 +0300

    Added report page

--- mvc/app/controllers/Reports.php
+++ mvc/app/controllers/Reports.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Created by PhpStorm.
  * User: Razvan Bordeianu
@@ -8,6 +7,49 @@
  */
 class Reports extends Controller
 {
+    public function reportslist($page=' ')
+    {
+        $this->view('reports/reports',[]);
+        return;
+    }
+    public function report($report_id=' '){
+        if(Report::checkReport($report_id)) {
 
+            $title=Report::getTitle($report_id);
+            $attSearC=Report::getsAttSpearCount($report_id);
+            $attAxeC=Report::getsAttAxeCount($report_id);
+            $attSwordC=Report::getsAttSwordCount($report_id);
+            $attArcherC=Report::getsAttArcherCount($report_id);
+            $attSpearD =Report::getsAttSpearDeadCount($report_id);
+            $attAxeD=Report::getsAttAxeDeadCount($report_id);
+            $attSwordD=Report::getsAttSwordDeadCount($report_id);
+            $attArcherD=Report::getsAttArcherDeadCount($report_id);
+            $defSpearC=Report::getsDefSpearCount($report_id);
+            $defAxeC=Report::getsDefAxeCount($report_id);
+            $defSwordC=Report::getsDefSwordCount($report_id);
+            $defArcherC=Report::getsDefArcherCount($report_id);
+            $defSpearD=Report::getsDefSpearDeadCount($report_id);
+            $defAxeD=Report::getsDefAxeDeadCount($report_id);
+            $defSwordD=Report::getsDefSwordDeadCount($report_id);
+            $defArcherD=Report::getsDefArcherDeadCount($report_id);
+            $attName=Report::getAttackerUsername($report_id);
+            $defName=Report::getDefenderUsername($report_id);
+            $attVillageName=Report::getAttackerVillageName($report_id);
+            $defVillageName=Report::getDefenderVillageName($report_id);
+            $msg=Report::getMessage($report_id);
+            $this->view('reports/actual-report',
+                ['title'=>$title,'attSpearC'=>$attSearC,'attAxeC'=>$attAxeC,'attSwordC'=>$attSwordC,'attArcherC'=>$attArcherC,
+                    'defSpearC'=>$defSpearC,'defAxeC'=>$defAxeC,'defSwordC'=>$defSwordC,'defArcherC'=>$defArcherC,
+                    'attSpearD'=>$attSpearD,'attAxeD'=>$attAxeD,'attSwordD'=>$attSwordD,'attArcherD'=>$attArcherD,
+                    'defSpearD'=>$defSpearD,'defAxeD'=>$defAxeD,'defSwordD'=>$defSwordD,'defArcherD'=>$defArcherD,
+                    'attName'=>$attName,'defName'=>$defName,'attVillageName'=>$attVillageName,'defVillageName'=>$defVillageName,
+                    'msg'=>$msg
+                    ]);
+        }
+        else{
+             header('Location: /Proiect-TW/mvc/public/reports/reportslist');
+        }
+        return;
+    }
 
 }
--- mvc/app/views/reports/reports.php
+++ mvc/app/views/reports/reports.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +11,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="css/reports.css">
+    <link rel="stylesheet" href="/Proiect-TW/mvc/public/css/reports.css">
 </head>
 <body>
 <nav class="navbar navbar-default">
@@ -18,18 +22,18 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-logo" href="info.html">
-                <img alt="Logo" src="images/logocrop60%25.png" class="img-responsive">
+            <a class="navbar-logo" href="/Proiect-TW/mvc/public/user/info">
+                <img alt="Logo" src="/Proiect-TW/mvc/public/images/logocrop60%25.png" class="img-responsive">
             </a>
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="PrivireAsupraSatului.html"><span class="glyphicon glyphicon-home"></span> Home </a></li>
-                <li><a href="profile.html"><span class="glyphicon glyphicon-user"></span> Profile </a></li>
-                <li><a href="map.html"><span class="glyphicon glyphicon-globe"></span> Map </a></li>
-                <li><a href="reports.html"><span class="glyphicon glyphicon-comment"></span> Reports <span class="badge">17</span> </a></li>
-                <li><a href="Ranking.html"><span class="glyphicon glyphicon-stats"></span> Ranking </a></li>
-                <li><a href="login.html"><span class="glyphicon glyphicon-off"></span> LogOut </a></li>
+                <li><a href="/Proiect-TW/mvc/public/village/PrivireAsupraSatului"><span class="glyphicon glyphicon-home"></span> Home </a></li>
+                <li><a href="/Proiect-TW/mvc/public/UserProfile/getProfile"><span class="glyphicon glyphicon-user"></span> Profile </a></li>
+                <li><a href="/Proiect-TW/mvc/public/map/getMap"><span class="glyphicon glyphicon-globe"></span> Map </a></li>
+                <li><a href="/Proiect-TW/mvc/public/reports/reportslist"><span class="glyphicon glyphicon-comment"></span> Reports <span class="badge">17</span> </a></li>
+                <li><a href="/Proiect-TW/mvc/public/ranking/Ranking"><span class="glyphicon glyphicon-stats"></span> Ranking </a></li>
+                <li><a href="/Proiect-TW/mvc/public"><span class="glyphicon glyphicon-off"></span> LogOut </a></li>
             </ul>
         </div>
     </div>
@@ -37,13 +41,13 @@
 
 <div class="center">
     <div class="row">
-        <div class="col-md-4 col-xs-2 col-sm-6 col-lg-6 Village">Village (23|99) </div>
+        <div class="col-md-4 col-xs-2 col-sm-6 col-lg-6 Village"><?php Functions::getVillageName($_SESSION['user_id']); ?></div>
         <div class="col md-2 col-xs-1 col-sm-2 col-lb-2"></div>
         <div class="col-md-6 col-xs-9 col-sm-4 col-lg-4" >
-            234 <img alt="Iron" src="images/iron.png">
-            543 <img alt="Wood" src="images/wood.png">
-            532 <img alt="Stone" src="images/stone.png">
-            1000<img alt="Resources" src="images/resources.png">
+            <?php Functions::getIronResources($_SESSION['village_name']);  ?> <img alt="Iron" src="/Proiect-TW/mvc/public/images/iron.png">
+            <?php Functions::getWoodResources($_SESSION['village_name']);  ?><img alt="Wood" src="/Proiect-TW/mvc/public/images/wood.png">
+            <?php Functions::getStoneResources($_SESSION['village_name']);  ?> <img alt="Stone" src="/Proiect-TW/mvc/public/images/stone.png">
+            1000<img alt="Resources" src="/Proiect-TW/mvc/public/images/resources.png">
         </div>
     </div>
     <div class="row">
@@ -62,43 +66,43 @@
                 </thead>
                 <tbody>
                 <tr>
-                    <td><a class="c-link" href="actual-report.html"><img alt="Attack" src="images/att.png">swarm l-a atacat pe **vodo**</a></td>
+                    <td><a class="c-link" href="/Proiect-TW/mvc/public/reports/report"><img alt="Attack" src="/Proiect-TW/mvc/public/images/att.png">swarm l-a atacat pe **vodo**</a></td>
                     <td>22/10/2016 20:34:00</td>
                 </tr>
                 <tr>
-                    <td><a class="c-link" href="actual-report.html"><img alt="Attack" src="images/att.png">swarm l-a atacat pe **vodo**</a></td>
+                    <td><a class="c-link" href="/Proiect-TW/mvc/public/reports/report"><img alt="Attack" src="/Proiect-TW/mvc/public/images/att.png">swarm l-a atacat pe **vodo**</a></td>
                     <td>22/10/2016 20:34:00</td>
                 </tr>
                 <tr>
-                    <td><a class="c-link" href="actual-report.html"><img alt="Attack" src="images/att.png">swarm l-a atacat pe **vodo**</a></td>
+                    <td><a class="c-link" href="/Proiect-TW/mvc/public/reports/report"><img alt="Attack" src="/Proiect-TW/mvc/public/images/att.png">swarm l-a atacat pe **vodo**</a></td>
                     <td>22/10/2016 20:34:00</td>
                 </tr>
                 <tr>
-                    <td><a class="c-link" href="actual-report.html"><img alt="Attack" src="images/att.png">swarm l-a atacat pe **vodo**</a></td>
+                    <td><a class="c-link" href="/Proiect-TW/mvc/public/reports/report"><img alt="Attack" src="/Proiect-TW/mvc/public/images/att.png">swarm l-a atacat pe **vodo**</a></td>
                     <td>22/10/2016 20:34:00</td>
                 </tr>
                 <tr>
-                    <td><a class="c-link" href="actual-report.html"><img alt="Attack" src="images/att.png">swarm l-a atacat pe **vodo**</a></td>
+                    <td><a class="c-link" href="/Proiect-TW/mvc/public/reports/report"><img alt="Attack" src="/Proiect-TW/mvc/public/images/att.png">swarm l-a atacat pe **vodo**</a></td>
                     <td>22/10/2016 20:34:00</td>
                 </tr>
                 <tr>
-                    <td><a class="c-link" href="actual-report.html"><img alt="Attack" src="images/att.png">swarm l-a atacat pe **vodo**</a></td>
+                    <td><a class="c-link" href="/Proiect-TW/mvc/public/reports/report"><img alt="Attack" src="/Proiect-TW/mvc/public/images/att.png">swarm l-a atacat pe **vodo**</a></td>
                     <td>22/10/2016 20:34:01</td>
                 </tr>
                 <tr>
-                    <td><a class="c-link" href="actual-report.html"><img alt="Attack" src="images/att.png">swarm l-a atacat pe **vodo**</a></td>
+                    <td><a class="c-link" href="/Proiect-TW/mvc/public/reports/report"><img alt="Attack" src="/Proiect-TW/mvc/public/images/att.png">swarm l-a atacat pe **vodo**</a></td>
                     <td>22/10/2016 20:34:01</td>
                 </tr>
                 <tr>
-                    <td><a class="c-link" href="actual-report.html"><img alt="Attack" src="images/att.png">swarm l-a atacat pe **vodo**</a></td>
+                    <td><a class="c-link" href="/Proiect-TW/mvc/public/reports/report"><img alt="Attack" src="/Proiect-TW/mvc/public/images/att.png">swarm l-a atacat pe **vodo**</a></td>
                     <td>22/10/2016 20:34:01</td>
                 </tr>
                 <tr>
-                    <td><a class="c-link" href="actual-report.html"><img alt="Attack" src="images/att.png">swarm l-a atacat pe **vodo**</a></td>
+                    <td><a class="c-link" href="/Proiect-TW/mvc/public/reports/report"><img alt="Attack" src="/Proiect-TW/mvc/public/images/att.png">swarm l-a atacat pe **vodo**</a></td>
                     <td>22/10/2016 20:34:01</td>
                 </tr>
                 <tr>
-                    <td><a class="c-link" href="actual-report.html"><img alt="Attack" src="images/att.png">swarm l-a atacat pe **vodo**</a></td>
+                    <td><a class="c-link" href="/Proiect-TW/mvc/public/reports/report"><img alt="Attack" src="/Proiect-TW/mvc/public/images/att.png">swarm l-a atacat pe **vodo**</a></td>
                     <td>22/10/2016 20:34:01</td>
                 </tr>
                 </tbody>
