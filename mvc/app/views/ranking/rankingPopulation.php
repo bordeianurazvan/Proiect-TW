142facfbea07711644ed8f1a487d18f0cd7d10c7
DamocValentin <valentin.damoc@yahoo.com>
2017-05-30 14:27:20 +0300

    Added Search Bar in Rankings

--- mvc/app/views/ranking/rankingPopulation.php
+++ mvc/app/views/ranking/rankingPopulation.php
@@ -89,9 +89,9 @@
     <div class="row">
         <div class="col-md-1 col-sm-1 col-xs-0"></div>
         <div class="col-md-6 col-xs-5 col-sm-6 col-lg-6">
-            <form class="navbar-form Search" role="search">
+            <form class="navbar-form Search" role="search" action="" method="post">
                 <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Search username">
+                    <input name="search" type="text" class="form-control" placeholder="Search username">
                 </div>
                 <button type="submit" class="btn btn-primary SearchButton">Search</button>
             </form>
