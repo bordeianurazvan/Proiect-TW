7dd40c8161184b6a227780b74945312c31be5cd5
DamocValentin <valentin.damoc@yahoo.com>
2017-06-01 11:58:16 +0300

    Fixed bug when listing the villages with 0 villages

--- mvc/app/controllers/Village.php
+++ mvc/app/controllers/Village.php
@@ -187,34 +187,38 @@ class Village extends Controller
     public function userVillages($id_user,$page='')
     {
         SessionValidate::validateSession();
-        if ($page > VillageFunctions::getNumberOfPages($id_user)) {
-            header('Location: /Proiect-TW/mvc/public/village/userVillages/' . $id_user . '/1');
-        } else {
-            if ($page < 1) {
+        if (VillageFunctions::getNumberOfVillages($id_user) > 0) {
+            if ($page > VillageFunctions::getNumberOfPages($id_user)) {
                 header('Location: /Proiect-TW/mvc/public/village/userVillages/' . $id_user . '/1');
             } else {
-                $username=VillageFunctions::getUsername($id_user);
-                $id=$id_user;
-                $currentPage = $page;
-                $nextPage = $page + 1;
-                $prevPage = $page - 1;
-                $maxPagesNumber = VillageFunctions::getNumberOfPages($id_user);
-                $villageList = VillageFunctions::getVillagesPage($page,$id_user);
-                $villageIdList = VillageFunctions::getVillagesIdPage($page,$id_user);
-                $coordsList = VillageFunctions::getVillagesCoordsPage($villageIdList);
-                $pointsList = VillageFunctions::getPointsPage($villageIdList);
-                $reportsCount = Report::getReportsCount($_SESSION['user_id']);
-                $village_name = VillageFunctions::getVillageNameById($_SESSION['village_id']);
-                $iron = VillageFunctions::getIronResources($_SESSION['village_id']);
-                $stone = VillageFunctions::getStoneResources($_SESSION['village_id']);
-                $wood = VillageFunctions::getWoodResources($_SESSION['village_id']);
-                $storage = VillageFunctions::getStorrageLevel($_SESSION['village_id']);
-                $this->view('village/userVillages', ['village_name' => $village_name, 'iron' => $iron,
-                    'stone' => $stone, 'wood' => $wood, 'storage' => $storage, 'reportsCount' => $reportsCount,
-                    'pointsList'=>$pointsList,'coordsList'=>$coordsList,'username'=>$username,
-                    'villageList'=>$villageList,'maxPagesNumber'=>$maxPagesNumber,'prevPage'=>$prevPage,
-                    'nextPage'=>$nextPage,'currentPage'=>$currentPage,'reportsCount'=>$reportsCount,'id'=>$id]);
+                if ($page < 1) {
+                    header('Location: /Proiect-TW/mvc/public/village/userVillages/' . $id_user . '/1');
+                } else {
+                    $username = VillageFunctions::getUsername($id_user);
+                    $id = $id_user;
+                    $currentPage = $page;
+                    $nextPage = $page + 1;
+                    $prevPage = $page - 1;
+                    $maxPagesNumber = VillageFunctions::getNumberOfPages($id_user);
+                    $villageList = VillageFunctions::getVillagesPage($page, $id_user);
+                    $villageIdList = VillageFunctions::getVillagesIdPage($page, $id_user);
+                    $coordsList = VillageFunctions::getVillagesCoordsPage($villageIdList);
+                    $pointsList = VillageFunctions::getPointsPage($villageIdList);
+                    $reportsCount = Report::getReportsCount($_SESSION['user_id']);
+                    $village_name = VillageFunctions::getVillageNameById($_SESSION['village_id']);
+                    $iron = VillageFunctions::getIronResources($_SESSION['village_id']);
+                    $stone = VillageFunctions::getStoneResources($_SESSION['village_id']);
+                    $wood = VillageFunctions::getWoodResources($_SESSION['village_id']);
+                    $storage = VillageFunctions::getStorrageLevel($_SESSION['village_id']);
+                    $this->view('village/userVillages', ['village_name' => $village_name, 'iron' => $iron,
+                        'stone' => $stone, 'wood' => $wood, 'storage' => $storage, 'reportsCount' => $reportsCount,
+                        'pointsList' => $pointsList, 'coordsList' => $coordsList, 'username' => $username,
+                        'villageList' => $villageList, 'maxPagesNumber' => $maxPagesNumber, 'prevPage' => $prevPage,
+                        'nextPage' => $nextPage, 'currentPage' => $currentPage, 'reportsCount' => $reportsCount, 'id' => $id]);
+                }
             }
+        }else{
+            header('Location: /Proiect-TW/mvc/public/ranking/rankingPopulation/1');
         }
     }
 
